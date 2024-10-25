e27be3746377e4b2aeeb0bca35134b72eb478a36
TATVN\anh.quach <anh.quach@tatmart.com>
2024-10-25 08:37:50 +0700

    đổi kiểu dữ liệu

--- src/app/api/sku/route.ts
+++ src/app/api/sku/route.ts
@@ -1,13 +1,11 @@
 import { NextResponse } from 'next/server';
 import { getConnection } from '@/lib/db';
-import {RowDataPacket} from "mysql2/promise";
-
 
 // API để lấy danh sách SKU có Active = 1
 export async function GET() {
   const connection = await getConnection();
   try {
-    const [rows]: [RowDataPacket[], any] = await connection.execute('SELECT * FROM product_skus WHERE active = 1');
+    const [rows] = await connection.execute('SELECT * FROM product_skus WHERE active = 1');
     const response = NextResponse.json({ message: 'Message successful' ,rows}, { status: 200 });
     return response;
   } catch (error) {
