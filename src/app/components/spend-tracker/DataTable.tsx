fa7e054aca93473d8dc1a4f0d25d9574d07cf474
TATVN\anh.quach <anh.quach@tatmart.com>
2024-11-13 11:38:32 +0700

    thêm part id

--- src/app/components/spend-tracker/DataTable.tsx
+++ src/app/components/spend-tracker/DataTable.tsx
@@ -4,6 +4,7 @@ import moment from 'moment';
 
 interface TableRow {
   id: number;
+  part_id: string;
   sku: string;
   rfid: string;
   qty: number;
@@ -21,6 +22,7 @@ const DataTable: React.FC<DataTableProps> = ({ data }) => {
       <table className="min-w-full bg-white border border-gray-200">
         <thead>
         <tr className="bg-gray-200">
+          <th className="py-2 px-4 border-b text-left">PART ID</th>
           <th className="py-2 px-4 border-b text-left">TAT SKU</th>
           <th className="py-2 px-4 border-b text-left">Container RFID</th>
           <th className="py-2 px-4 border-b text-left">Qty/Container</th>
@@ -32,6 +34,11 @@ const DataTable: React.FC<DataTableProps> = ({ data }) => {
         {data.length > 0 ? (
           data.map((row) => (
             <tr key={row.id}>
+              <td
+                className={`py-2 px-4 border-b ${row.status === 'Empty' ? 'bg-yellow-300' : ''}`}
+              >
+                {row.part_id}
+              </td>
               <td
                 className={`py-2 px-4 border-b ${row.status === 'Empty' ? 'bg-yellow-300' : ''}`}
               >
